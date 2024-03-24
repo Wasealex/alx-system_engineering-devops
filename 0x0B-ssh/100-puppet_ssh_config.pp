204e1f1a8d88cd91e478b81d25639f639c25b79b
Wasealex <wasealex27@gmail.com>
2024-03-24 16:05:01 +0300

    trial 4

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,14 +1,16 @@
 #create ssh config file
 #configuration must be configured to use the private key ~/.ssh/school
 #configuration must be configured to refuse to authenticate using a password
-file_line { 'Disable password authentication':
-  ensure => 'present',
-  path   => "etc/ssh/config",
-  line   => '    PasswordAuthentication no',
-}
-
-file_line { 'Specify identity file':
-  ensure => 'present',
-  path   => "etc/ssh/config",
-  line   => '    IdentityFile ~/.ssh/school',
+class ssh_config {
+  file { '/home/vagrant/.ssh/config':
+    ensure  => present,
+    mode    => '0600',
+    content => "
+      Host your_server
+        IdentityFile ~/.ssh/school
+        PasswordAuthentication no
+    ",
+    owner   => 'vagrant',
+    group   => 'vagrant',
+  }
 }
