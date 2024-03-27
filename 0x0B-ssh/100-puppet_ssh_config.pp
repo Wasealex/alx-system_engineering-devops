4d6d777d56316d8d47f18038d7d519fbebd0ee6a
Wasealex <wasealex27@gmail.com>
2024-03-27 09:44:32 +0300

    trial3

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,9 +1,14 @@
 #create ssh config file
 #configuration must be configured to use the private key ~/.ssh/school
 #configuration must be configured to refuse to authenticate using a password
-file {'/etc/ssh/ssh_config':
-  ensure  => present,
-  content => "Host *
-                   IndentifyFile ~/.ssh/school
-                   PasswordAuthentication no",
+file_line { 'change identity_file':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '   IdentityFile ~/.ssh/school',
+}
+
+file_line { 'turn off pwd auth':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '   PasswordAuthentication no',
 }
