825c76878d934848ab7c7db53e378e29ccfe23f7
Wasealex <wasealex27@gmail.com>
2024-05-09 01:37:06 +0300

    trial3

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 #to fix apache2 php setting
 exec { ' fix it ':
-  command => '/usr/bin/sed -i "s/phpp/php/" /var/www/html/wp-settings.php',
+  command => 'sed -i "s/phpp/php/" /var/www/html/wp-settings.php',
   path    => ['/usr/bin', '/bin'],
 }
