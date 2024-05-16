1a0039f977cbed1ce1be782315eb6eb76b763063
Wasealex <wasealex27@gmail.com>
2024-05-16 22:22:35 +0300

    puppet-lint fix

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,8 +1,9 @@
-# Increasing the ammount of traffic on Nginx server
+# Increasing the amount of traffic on Nginx server
+
 exec { 'fixnginx':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
-  path    => '/usr/local/bin/:/bin/'
-} ->
-exec { 'nginx-restart':
-  command => 'nginx restart',
-  path    => '/etc/init.d/'
+  path    => '/usr/local/bin/:/bin/',
+} -> exec { 'nginx-restart':
+  command => '/etc/init.d/nginx restart',
+  path    => '/etc/init.d/',
+}
